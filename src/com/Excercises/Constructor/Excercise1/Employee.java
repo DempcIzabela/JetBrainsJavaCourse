1a686a0545a979c78529416e9bd8299d30aca248
Iza Dempc <dempcizabela@gmail.com>
2020-04-27 19:50:33 +0200

    Excercise 1 from Constructors

---
+++ src/com/Excercises/Constructor/Excercise1/Employee.java
@@ -0,0 +1,21 @@
+package com.Excercises.Constructor.Excercise1;
+
+class Employee {
+
+    String name;
+    int salary;
+    String address;
+    Employee(){
+        this("unknown",0);
+    }
+    Employee(String name, int salary){
+        this(name, salary, "unknown");
+    }
+    Employee(String name, int salary, String address){
+        this.name = name;
+        this.salary = salary;
+        this.address = address;
+    }
+
+}
+
