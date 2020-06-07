1d0eb9e875255c8e634e76d0263f94a9429e6a50
Iza Dempc <dempcizabela@gmail.com>
2020-06-07 11:26:34 +0200

    excercise 1 from converting

---
+++ src/com/Excercises/convert/Excercise1/Main.java
@@ -0,0 +1,34 @@
+package com.Excercises.convert.Excercise1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int convert(Long val) {
+
+        if(val == null){
+            return 0;
+        }
+        else{
+            int val_int = val.intValue();
+            if(val>Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+            else if (val<Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+            else {
+                return val_int;
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String val = scanner.nextLine();
+        Long longVal = "null".equals(val) ? null : Long.parseLong(val);
+        System.out.println(convert(longVal));
+    }
+}
